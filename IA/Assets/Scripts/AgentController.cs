90fdb3ee739b5f9cf8b54018b6eb032f00d7215b
FigDev <twofigsthree@gmail.com>
2024-10-19 04:03:04 -0500

    Proyecto_Init
    Primera subida con un modelo de IA basico entrenado, y con todas las dependecias necesarias

---
+++ IA/Assets/Scripts/AgentController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class AgentController : Agent 
+{
+
+    [SerializeField] private Transform target;
+    [SerializeField] private float moveSpeed = 4f;
+
+    public override void OnEpisodeBegin()
+    {
+        transform.localPosition = new Vector3(Random.Range(-4f,4f), 0.3f, Random.Range(-4f, 4f));
+
+        target.localPosition = new Vector3(Random.Range(-4f, 4f), 0.3f, Random.Range(-4f, 4f));
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(target.localPosition);
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        float moveX = actions.ContinuousActions[0];
+        float moveZ = actions.ContinuousActions[1];
+
+        Vector3 velocity = new Vector3(moveX, 0f, moveZ);
+        velocity = velocity.normalized * Time.deltaTime * moveSpeed;
+
+        transform.localPosition += velocity;
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        continuousActions[0] = Input.GetAxisRaw("Horizontal");
+        continuousActions[1] = Input.GetAxisRaw("Vertical");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Coin"))
+        {
+            AddReward(10f);
+            EndEpisode();
+        }
+        if(other.gameObject.CompareTag("Wall"))
+        {
+            AddReward(-1f);
+            EndEpisode();
+        }
+
+    }
+
+
+}
